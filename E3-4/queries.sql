57ab77e06363202eeb03680d4d235ecce8ae83ac
Arianna Masciolini <uzkamascio@gmail.com>
2017-10-22 15:29:17 +0200

    Lesson 3-4: more queries.

--- E3-4/queries.sql
+++ E3-4/queries.sql
@@ -1,10 +1,53 @@
+--Qual è il contenuto della tabella 'persona'?
 SELECT * FROM persona;
-SELECT cognome FROM persona;
-SELECT cognome FROM persona ORDER BY cognome;
-SELECT DISTINCT cognome FROM persona;
-SELECT cognome FROM persona WHERE nome='John';
-SELECT DISTINCT * FROM persona WHERE id_attore=id_persona;
-SELECT cognome, nome, ruolo FROM persona, partecipazione, film WHERE film.id_film=partecipazione.id_film AND id_persona= id_attore;
-SELECT DISTINCT titolo from film WHERE genere='Drammatico';
-SELECT titolo FROM film, proiezione WHERE film.id_film=proiezione.id_film AND giorno>='2002-1-1' AND giorno<='2002-12-1';
 
+ --Selezionare i cognomi delle persone nel DB (senza eliminare i doppioni)
+SELECT cognome FROM persona;
+
+--Idem come sopra, con ordine lessicografico per cognome
+SELECT cognome FROM persona ORDER BY cognome;
+
+--Definire l'insieme dei cognomi (ossia idem come riga 2, eliminando i doppioni)
+SELECT DISTINCT cognome FROM persona;
+
+--Selezionare gli id delle persone di nome 'John'
+SELECT id_persona FROM persona WHERE nome='John';
+
+--Selezionare i cognomi delle persone di nome 'John'
+SELECT cognome FROM persona WHERE nome='John'
+
+--Selezionare gli id degli attori
+SELECT id_persona FROM persona, partecipazione WHERE id_attore=id_persona;
+
+--Definire la lista di tutte le interpretazioni, specificando cognome, nome, ruolo e titolo
+SELECT cognome, nome, ruolo, titolo FROM persona, partecipazione, film WHERE film.id_film=partecipazione.id_film AND id_persona= id_attore;
+
+--Idem come sopra, ma in modo discorsivo
+SELECT nome || ' ' || cognome || ' ha interpretato il ruolo di '  || ruolo || ' nel film ' || titolo FROM persona, partecipazione, film WHERE film.id_film=partecipazione.id_film AND id_persona= id_attore;
+
+--Quali film sono drammatici?
+SELECT DISTINCT titolo FROM film WHERE genere='Drammatico';
+
+--Quali titoli sono stati proiettati nel 2002?
+SELECT DISTINCT titolo FROM film, proiezione WHERE film.id_film=proiezione.id_film AND giorno>='2002-1-1' AND giorno<='2002-12-1';
+
+--Selezionare i film diretti da Lars von Trier
+SELECT DISTINCT titolo FROM film, persona where id_regista=id_persona AND nome='Lars' AND cognome='von Trier';
+
+--Elencare i titoli proiettati al cinema 'Le Fontanelle'
+SELECT DISTINCT titolo FROM film, proiezione, cinema WHERE film.id_film=proiezione.id_film AND proiezione.id_cinema=cinema.id_cinema and cinema.nome='Le Fontanelle';
+
+--Elencare nome e cognome di tutti i registi
+SELECT DISTINCT nome, cognome FROM persona, film WHERE id_persona=id_regista;
+
+--Elencare nome e cognome di tutti gli attori
+SELECT DISTINCT nome, cognome FROM persona, partecipazione WHERE id_persona=id_attore;
+
+--Elencare nome e cognome di tutti gli attori che sono anche registi
+SELECT DISTINCT nome, cognome FROM persona, partecipazione, film WHERE id_persona=id_attore AND id_persona=id_regista;
+
+ --Quali attori hanno preso parte a film proiettati al cinema 'Zenith' dopo il 2000?
+SELECT DISTINCT nome, cognome FROM persona, partecipazione, proiezione WHERE id_persona=id_attore AND partecipazione.id_film=proiezione.id_film AND giorno>='2002-1-1';
+
+--Quali sono i titoli dei film con Nicole Kidman che non sono stati proiettati al cinema "Le Fontanelle"?
+SELECT DISTINCT film.titolo FROM film, persona, proiezione, partecipazione, cinema WHERE partecipazione.id_film=proiezione.id_film AND persona.id_persona=partecipazione.id_attore AND cinema.id_cinema=proiezione.id_cinema AND film.id_film=proiezione.id_film AND film.id_film=partecipazione.id_film AND cinema.nome!='Le Fontanelle';
